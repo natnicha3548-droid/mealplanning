100d36bad88f8df3e823f3d151dae63cf5d03cd6
Kanida-66 <sb6640248101@lru.ac.th>
2026-05-06 15:10:39 +0700

    ..

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,7 +52,7 @@ const Navbar = ({ user, setUser, setCalcResult }) => {
                     </li>
 
                     <li>
-                        <Link to="/meal-plans" onClick={handleClose}>
+                        <Link to="/meal-plan" onClick={handleClose}>
                             แผนการกินของฉัน
                         </Link>
                     </li>
